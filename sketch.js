3bcbae1d5c396ee9f5ad903af08241a0810f006a
Avra Goldman <avragoldman@gmail.com>
2018-06-17 15:39:27 -0700

    made colors for dots
    added if else for vaiables

--- sketch.js
+++ sketch.js
@@ -31,6 +31,7 @@ function setup() {
 
   background(0, 0, 0)
 
+//make draw function
   var tree_rows = table_trees.getRows()
   for (var r = 0; r < tree_rows.length; r++) {
     // choosing the location of the dot
@@ -41,12 +42,19 @@ function setup() {
     var species = tree_rows[r].getString("Species");
     //console.log(species);
     var directory_row = table_directory.findRow(species,"Scientific Name");
-  
+
     if(directory_row!==null){
       if(directory_row.getString("Fruit")==="showy") {
-        fill(254, 127, 146, 10);
-      } else {
-        fill(75, 250, 20, 10);
+        fill(254, 127, 146); //pink
+      }
+      else if(directory_row.getString("Bloom")==="showy") {
+        fill(0,191,255); //pink
+      }
+      else if(directory_row.getString("Drought-Tolerant?")==="yes") {
+        fill(255,255,255); //pink
+      }
+      else {
+        fill(75, 250, 20); //green
       }
     }
 
@@ -54,7 +62,11 @@ function setup() {
     // drawing the dot
     var x = map(lon, westernnmost_lat_of_sf, easternmost_lat_of_sf, 0, width)
     var y = map(lat, southernmost_lon_of_sf, northernmost_lon_of_sf, height, 0)
-    ellipse(x, y, 3, 3)
+    ellipse(x, y, 1, 1)
   }
 
 }
+
+//look up button to select keys
+//make variable -false, True
+//turn if else statements into if variable
